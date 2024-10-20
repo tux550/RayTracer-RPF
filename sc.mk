353fee55c930f30445c341f0a6f96fbc50e4a0d3
Rodrigo Salazar <rodrigosalad@gmail.com>
2024-10-20 00:47:15 -0500

    make

---
+++ sc.mk
@@ -0,0 +1,14 @@
+IMAGE_NAME = pbrt-v3
+IMAGE_VERSION = 1.0
+IMAGE_TAG = $(IMAGE_NAME):$(IMAGE_VERSION)
+
+DEV_CONTAINER_NAME = pbrt-v3-dev
+
+CONTAINER_WORK_DIR = /app
+HOST_OUTPUT_DIR = $(shell pwd)/output
+CONTAINER_OUTPUT_DIR = /app/output
+
+build-image:
+	docker build -t $(IMAGE_TAG) .
+shell:
+	docker run -it --rm -w $(CONTAINER_WORK_DIR) -v $(HOST_OUTPUT_DIR):$(CONTAINER_OUTPUT_DIR) $(IMAGE_TAG)  /bin/bash
